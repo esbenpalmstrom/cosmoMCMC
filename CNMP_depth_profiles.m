153908037375d351acbf2c1be4283e11137aea4f
esbenpalmstrom <esben@192.168.0.101>
2021-03-09 17:09:10 +0100

    added file for calculating and plotting CN production from muons

---
+++ CNMP_depth_profiles.m
@@ -0,0 +1,9 @@
+%CNMP_depth_profiles
+%esben v1
+%plot production rate of cosmogenic nuclides from fast muons and negative
+%muon capture
+
+%create depth profile
+
+
+%calculate production rate at each depth point
